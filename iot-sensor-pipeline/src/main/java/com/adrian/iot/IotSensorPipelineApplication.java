d0e7496ba197199aa6b1e3012724ac82e324e33b
Adrian Joseph Santoso <adrrii.joseph07@gmail.com>
2026-06-04 19:13:13 +0700

    feat: init spring boot project with dependencies

---
+++ iot-sensor-pipeline/src/main/java/com/adrian/iot/IotSensorPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSensorPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSensorPipelineApplication.class, args);
+	}
+
+}
